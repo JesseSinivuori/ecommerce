7cc987b7c1e2249010a8986db517767ff63559d2
JesseSinivuori <sinvuorii@gmail.com>
2023-07-27 18:18:02 +0300

    - build script

--- playwright.config.ts
+++ playwright.config.ts
@@ -90,7 +90,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: !process.env.CI ? "npm run dev" : "npm run start",
+    command: "npm run dev",
     url: baseURL,
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
